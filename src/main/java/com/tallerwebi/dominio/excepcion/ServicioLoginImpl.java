1d1ab97657fa9dd9230c0181d4392eda8df8e987
Fernando <fer120793@gmail.com>
2024-08-18 20:32:17 -0300

    Inicio

---
+++ src/main/java/com/tallerwebi/dominio/excepcion/ServicioLoginImpl.java
@@ -0,0 +1,37 @@
+package com.tallerwebi.dominio.excepcion;
+
+import com.tallerwebi.dominio.RepositorioUsuario;
+import com.tallerwebi.dominio.ServicioLogin;
+import com.tallerwebi.dominio.Usuario;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service("servicioLogin")
+@Transactional
+public class ServicioLoginImpl implements ServicioLogin {
+
+    private RepositorioUsuario repositorioUsuario;
+
+    @Autowired
+    public ServicioLoginImpl(RepositorioUsuario repositorioUsuario){
+        this.repositorioUsuario = repositorioUsuario;
+    }
+
+    @Override
+    public Usuario consultarUsuario (String email, String password) {
+        return repositorioUsuario.buscarUsuario(email, password);
+    }
+
+    @Override
+    public void registrar(Usuario usuario) throws UsuarioExistente {
+        Usuario usuarioEncontrado = repositorioUsuario.buscarUsuario(usuario.getEmail(), usuario.getPassword());
+        if(usuarioEncontrado != null){
+            throw new UsuarioExistente();
+        }
+        repositorioUsuario.guardar(usuario);
+    }
+
+}
+
